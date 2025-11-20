d70e63d17b864639bf880d947f3e5b5c4b41ef91
Abdel505 <abdelalouan07@gmail.com>
2025-11-20 15:01:21 +0100

    Test Calculating Parking Fee in Weekend sromano-courses#3

--- test/test_parking_garage.py
+++ test/test_parking_garage.py
@@ -38,3 +38,11 @@ class TestParkingGarage(TestCase):
         entry_time = datetime(2025, 11, 20, 12, 30)
         fee = garage.calculate_parking_fee(entry_time)
         self.assertEqual(7.5, fee)
+
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_calculate_parking_fee_in_weekend(self, rtc: Mock):
+        rtc.return_value = datetime(2025, 22, 20, 15, 30)
+        garage = ParkingGarage()
+        entry_time = datetime(2025, 22, 20, 12, 30)
+        fee = garage.calculate_parking_fee(entry_time)
+        self.assertEqual(9.375, fee)
